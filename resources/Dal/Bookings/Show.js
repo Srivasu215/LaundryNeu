904af3080d236b33c4caa4626fdbf52642753898
keshavsoft <nknnkumar@yahoo.com>
2022-09-27 08:12:06 +0530

    keshav

--- resources/Dal/Bookings/Show.js
+++ resources/Dal/Bookings/Show.js
@@ -26,33 +26,34 @@ let ShowTodayFunc = async () => {
     let LocalCustomersData = await Neutralino.filesystem.readFile(`./KData/JSON/2017/${LocalJsonFileName}`);
     let LocalCustomersDataAsJson = JSON.parse(LocalCustomersData);
 
-    Object.entries(LocalCustomersDataAsJson).forEach(
-        ([LoopKey, LoopValue]) => {
-            if ('DataTime' in LoopValue) {
-                let LoopRowDataTime = LoopValue.DataTime.substring(0, 10);
-                let LocalSysDateTime = LocalGetDateOnly();
+    let LocalCollectionData = Object.keys(LocalCustomersDataAsJson).map(key => ({ key, value: LocalCustomersDataAsJson[key] }));
 
-                if (LoopRowDataTime === LocalSysDateTime) {
+    let LocalFilteredData = _.filter(LocalCollectionData, (LoopItem) => {
+        return LoopItem.value.DateTime.substring(0, 10) === LocalGetDateOnly();
+    });
 
-                    let LoopNewObject = JSON.parse(JSON.stringify(ModalDataAsJson));
-                    let LocalLoopObject = {};
-
-                    Object.entries(LoopNewObject).forEach(
-                        ([key, value]) => {
-                            LocalLoopObject[key] = LoopValue[key];
-                            value = LoopValue[key];
-                        }
-                    );
-
-                    LocalReturnObject.JsonData[LoopKey] = LocalLoopObject;
-                };
-
-            };
-
-        }
-    );
-
+    LocalReturnObject.JsonData = LocalFilteredData;
     return await LocalReturnObject;
+};
+
+let ToStart = async () => {
+    let LocalJsonFileName = "Bookings.json";
+
+    let LocalReturnObject = { KTF: false, KResult: "", JsonData: {} };
+
+    let ModalData = await Neutralino.filesystem.readFile(`./KData/JSON/TemplateData/${LocalJsonFileName}`);
+    let ModalDataAsJson = JSON.parse(ModalData);
+
+    let LocalCustomersData = await Neutralino.filesystem.readFile(`./KData/JSON/2017/${LocalJsonFileName}`);
+    let LocalCustomersDataAsJson = JSON.parse(LocalCustomersData);
+
+    let LocalCollectionData = Object.keys(LocalCustomersDataAsJson).map(key => ({ key, value: LocalCustomersDataAsJson[key] }));
+
+    let LocalFilteredData = _.filter(LocalCollectionData, (LoopItem) => {
+        return (LoopItem.value.WashingDone.KTF === true) === false;
+    });
+
+    return await LocalFilteredData;
 };
 
 let ShowWashed = async () => {
